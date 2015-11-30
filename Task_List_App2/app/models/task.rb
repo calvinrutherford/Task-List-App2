3480bd4395bf0c026720437ddf8bc08642b8c480
Jonathan Arp <jarp@nd.edu>
2015-11-29 20:15:40 -0500

    notes

--- Task_List_App2/app/models/task.rb
+++ Task_List_App2/app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ActiveRecord::Base
 	belongs_to :TaskList
-
+	# jarp: should be 'belongs_to :task_list'
+	# 			there seems to be a problem with the association between task and task list
+	# 			The database column is capitalized and it shouldn't be. Works one way but there are issues when you try and access task_list from the task object
 	validates :title, :priority, :duedate, :Task_List_id, presence: { message: "must be present" }
-
   validates :priority, inclusion: { in: 1..10, message: 'needs to be between 1 and 10'  }
 end
--- Task_List_App2/app/models/task_list.rb
+++ Task_List_App2/app/models/task_list.rb
@@ -2,7 +2,7 @@ class TaskList < ActiveRecord::Base
 		has_many :tasks
 
 
-		validates :name, :description, presence: { message: "must be present" }
+	validates :name, :description, presence: { message: "must be present" }
   validates :name, uniqueness: true
-  
+
 end
